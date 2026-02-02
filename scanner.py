0a5f6398e26e70777c9519765d3e81f34c389ca0
Emmanuel Ohiren Akhibi <121525211+beeshawpmilo@users.noreply.github.com>
2026-02-02 18:15:49 +0100

    scanner.py

--- scanner.py
+++ scanner.py
@@ -384,8 +384,5 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print(f"\n{Fore.RED}Scan interrupted by user{Style.RESET_ALL}")
         sys.exit()
-```
     return filename
 ```
-
----
